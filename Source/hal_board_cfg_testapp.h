41a528c8b0bbdbf3b2a0d2aa24f4470a736fc871
Utyf <utyff@mail.ru>
2022-02-10 08:19:07 +0300

    merged hal_board_cfg.h from reference one

--- Source/hal_board_cfg_testapp.h
+++ Source/hal_board_cfg_testapp.h
@@ -1,24 +1,90 @@
+/**************************************************************************************************
+  Filename:       hal_board_cfg.h
+  Revised:        $Date: 2015-02-17 14:17:44 -0800 (Tue, 17 Feb 2015) $
+  Revision:       $Revision: 42683 $
+
+  Description:    Declarations for the CC2530EM used on the SmartRF05EB.
+
+
+  Copyright 2009-2015 Texas Instruments Incorporated. All rights reserved.
+
+  IMPORTANT: Your use of this Software is limited to those specific rights
+  granted under the terms of a software license agreement between the user
+  who downloaded the software, his/her employer (which must be your employer)
+  and Texas Instruments Incorporated (the "License"). You may not use this
+  Software unless you agree to abide by the terms of the License. The License
+  limits your use, and you acknowledge, that the Software may not be modified,
+  copied or distributed unless embedded on a Texas Instruments microcontroller
+  or used solely and exclusively in conjunction with a Texas Instruments radio
+  frequency transceiver, which is integrated into your product. Other than for
+  the foregoing purpose, you may not use, reproduce, copy, prepare derivative
+  works of, modify, distribute, perform, display or sell this Software and/or
+  its documentation for any purpose.
+
+  YOU FURTHER ACKNOWLEDGE AND AGREE THAT THE SOFTWARE AND DOCUMENTATION ARE
+  PROVIDED “AS IS” WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+  INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF MERCHANTABILITY, TITLE,
+  NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL
+  TEXAS INSTRUMENTS OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER CONTRACT,
+  NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR OTHER
+  LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+  INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE
+  OR CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT
+  OF SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+  (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+
+  Should you have any questions regarding your right to use this Software,
+  contact Texas Instruments Incorporated at www.TI.com.
+**************************************************************************************************/
+
 #ifndef HAL_BOARD_CFG_H
 #define HAL_BOARD_CFG_H
+
+
+/* ------------------------------------------------------------------------------------------------
+ *                                           Includes
+ * ------------------------------------------------------------------------------------------------
+ */
 
 #include "hal_mcu.h"
 #include "hal_defs.h"
 #include "hal_types.h"
 
-// HAL_PA_LNA - для CC2530+CC2591EM
-// HAL_PA_LNA_CC2590 - для CC2530+CC2590EM
-// HAL_PA_LNA_CC2592 - для CC2530+CC2592EM
-// Только одна из опций может быть задана
+/* ------------------------------------------------------------------------------------------------
+ *                                       CC2590/CC2591 support
+ *
+ *                        Define HAL_PA_LNA_CC2590 if CC2530+CC2590EM is used
+ *                        Define HAL_PA_LNA if CC2530+CC2591EM is used
+ *                        Note that only one of them can be defined
+ * ------------------------------------------------------------------------------------------------
+ */
 #define xHAL_PA_LNA
 #define xHAL_PA_LNA_CC2590
 #define xHAL_PA_LNA_SE2431L
 #define xHAL_PA_LNA_CC2592
 
 
-// Частота процессора
+/* ------------------------------------------------------------------------------------------------
+ *                                       Board Indentifier
+ *
+ *      Define the Board Identifier to HAL_BOARD_CC2530EB_REV13 for SmartRF05 EB 1.3 board
+ * ------------------------------------------------------------------------------------------------
+ */
+
+//#if !defined (HAL_BOARD_CC2530EB_REV17) && !defined (HAL_BOARD_CC2530EB_REV13)
+//#define HAL_BOARD_CC2530EB_REV17
+//#endif
+
+/* ------------------------------------------------------------------------------------------------
+ *                                          Clock Speed
+ * ------------------------------------------------------------------------------------------------
+ */
+
 #define HAL_CPU_CLOCK_MHZ     32
 
-// Флаг наличия кварца
+/* This flag should be defined if the SoC uses the 32MHz crystal
+ * as the main clock source (instead of DCO).
+ */
 #ifdef HAL_SONOFF
 //#define HAL_CLOCK_CRYSTAL
   #define OSC32K_CRYSTAL_INSTALLED FALSE
@@ -28,15 +94,17 @@
 
 /* 32 kHz clock source select in CLKCONCMD */
 #if !defined (OSC32K_CRYSTAL_INSTALLED) || (defined (OSC32K_CRYSTAL_INSTALLED) && (OSC32K_CRYSTAL_INSTALLED == TRUE))
-#define OSC_32KHZ  0x00 /* external 32 KHz xosc */
+  #define OSC_32KHZ  0x00 /* external 32 KHz xosc */
 #else
-#define OSC_32KHZ  0x80 /* internal 32 KHz rcosc */
+  #define OSC_32KHZ  0x80 /* internal 32 KHz rcosc */
 #endif
 
 #define HAL_CLOCK_STABLE()    st( while (CLKCONSTA != (CLKCONCMD_32MHZ | OSC_32KHZ)); )
 
-
-// Конфигурация светодиодов
+/* ------------------------------------------------------------------------------------------------
+ *                                       LED Configuration
+ * ------------------------------------------------------------------------------------------------
+ */
 
 // Количество светодиодов
 #define HAL_NUM_LEDS            3
@@ -44,16 +112,16 @@
 #define HAL_LED_BLINK_DELAY()   st( { volatile uint32 i; for (i=0; i<0x5800; i++) { }; } )
 
 #ifdef HAL_SONOFF
-/* 1 - P0_7 Реле */
-  #define LED1_BV           BV(7)
-  #define LED1_SBIT         P0_7
+/* 1 - P0_1 Реле */
+  #define LED1_BV           BV(1)
+  #define LED1_SBIT         P0_1
   #define LED1_DDR          P0DIR
   #define LED1_POLARITY     ACTIVE_HIGH
 
-  /* 2 - P1_0 Синий */
+  /* 2 - P0_0 Синий */
   #define LED2_BV           BV(0)
-  #define LED2_SBIT         P1_0
-  #define LED2_DDR          P1DIR
+  #define LED2_SBIT         P0_0
+  #define LED2_DDR          P0DIR
   #define LED2_POLARITY     ACTIVE_HIGH
 #else
 /* 1 - P1_0 Зеленый */
@@ -81,7 +149,10 @@
 #define LED4_POLARITY
 #define LED4_SET_DIR()
 
-// Конфигурация кнопок
+/* ------------------------------------------------------------------------------------------------
+ *                                    Push Button Configuration
+ * ------------------------------------------------------------------------------------------------
+ */
 
 #define ACTIVE_LOW        !
 #define ACTIVE_HIGH       !!    /* double negation forces result to be '1' */
@@ -130,8 +201,10 @@
 #define TSENS_BV BV(1)
 #define TSENS_DIR P2DIR
 
-
-// OSAL NV - постоянная внутренняя flash-память
+/* ------------------------------------------------------------------------------------------------
+ *                         OSAL NV implemented by internal flash pages.
+ * ------------------------------------------------------------------------------------------------
+ */
 
 // Flash is partitioned into 8 banks of 32 KB or 16 pages.
 #define HAL_FLASH_PAGE_PER_BANK    16
@@ -174,7 +247,6 @@
 #define HAL_NV_DMA_GET_DESC()      HAL_DMA_GET_DESC0()
 #define HAL_NV_DMA_SET_ADDR(a)     HAL_DMA_SET_ADDR_DESC0((a))
 
-
 /* ------------------------------------------------------------------------------------------------
  *  Serial Boot Loader: reserving the first 4 pages of flash and other memory in cc2530-sb.xcl.
  * ------------------------------------------------------------------------------------------------
@@ -185,8 +257,12 @@
 // Size of internal flash less 4 pages for boot loader, 6 pages for NV, & 1 page for lock bits.
 #define HAL_SB_IMG_SIZE      (0x40000 - 0x2000 - 0x3000 - 0x0800)
 
+/* ------------------------------------------------------------------------------------------------
+ *                                            Macros
+ * ------------------------------------------------------------------------------------------------
+ */
 
-// Инициализация RF-frontend усилителя
+/* ----------- RF-frontend Connection Initialization ---------- */
 #if defined HAL_PA_LNA || defined HAL_PA_LNA_CC2590 || \
     defined HAL_PA_LNA_SE2431L || defined HAL_PA_LNA_CC2592
 extern void MAC_RfFrontendSetup(void);
@@ -202,7 +278,7 @@ extern void MAC_RfFrontendSetup(void);
 // Внешняя функция инициализации кнопок
 extern void TestApp_HalKeyInit( void );
 
-// Инициализация оборудования (для модулей без усилителей)
+/* ----------- Board Initialization ---------- */
 #if !defined (HAL_PA_LNA) && \
     !defined (HAL_PA_LNA_CC2590) && !defined (HAL_PA_LNA_SE2431L) && \
     !defined (HAL_PA_LNA_CC2592)
@@ -289,27 +365,26 @@ extern void TestApp_HalKeyInit( void );
   /* set direction for GPIO outputs  */                          \
   /* For SE2431L PA LNA this sets ANT_SEL to output */           \
   /* For CC2592 this enables LNA */                              \
-  P1DIR |= BV(0);                                                \
+  P1DIR |= BV(0) | BV(1);                                        \
                                                                  \
-  LED3_DDR |= LED3_BV;                                           \
+  /*LED3_DDR |= LED3_BV; */                                         \
                                                                  \
   /* Set PA/LNA HGM control P0_7 */                              \
   P0DIR |= BV(7);                                                \
                                                                  \
   /* configure tristates */                                      \
-  P0INP |= PUSH2_BV;                                             \
-  TestApp_HalKeyInit();                                         \
+/*  P0INP |= PUSH2_BV;                                             \
+  TestApp_HalKeyInit(); */                                        \
                                                                  \
   /* setup RF frontend if necessary */                           \
   HAL_BOARD_RF_FRONTEND_SETUP();                                 \
 }
 #endif
 
-
-// Макрос защиты от дребезга контактов
+/* ----------- Debounce ---------- */
 #define HAL_DEBOUNCE(expr)    { int i; for (i=0; i<500; i++) { if (!(expr)) i = 0; } }
 
-// Макросы для проверки кнопок
+/* ----------- Push Buttons ---------- */
 #define HAL_PUSH_BUTTON1()        (PUSH1_POLARITY (PUSH1_SBIT))
 #define HAL_PUSH_BUTTON2()        (PUSH2_POLARITY (PUSH2_SBIT))
 #define HAL_PUSH_BUTTON3()        (0)
@@ -317,7 +392,7 @@ extern void TestApp_HalKeyInit( void );
 #define HAL_PUSH_BUTTON5()        (0)
 #define HAL_PUSH_BUTTON6()        (0)
 
-// Макросы для управления светодиодами
+/* ----------- LED's ---------- */
 #define HAL_TURN_OFF_LED1()       st( LED1_SBIT = LED1_POLARITY (0); )
 #define HAL_TURN_OFF_LED2()       st( LED2_SBIT = LED2_POLARITY (0); )
 #define HAL_TURN_OFF_LED3()       st( LED3_SBIT = LED3_POLARITY (0); )
@@ -389,30 +464,32 @@ st( \
 #define VDD_MIN_GOOD (VDD_2_0+8)  // 10% margin over minimum to survive a page erase and compaction.
 #define VDD_MIN_XNV  (VDD_2_7+5)  // 5% margin over minimum to survive a page erase and compaction.
 
+/* ------------------------------------------------------------------------------------------------
+ *                                     Driver Configuration
+ * ------------------------------------------------------------------------------------------------
+ */
 
-// Конфигурация драйвера
-
-// Аппаратные таймеры
+/* Set to TRUE enable H/W TIMER usage, FALSE disable it */
 #ifndef HAL_TIMER
 #define HAL_TIMER FALSE
 #endif
 
-// Использование ADC
+/* Set to TRUE enable ADC usage, FALSE disable it */
 #ifndef HAL_ADC
 #define HAL_ADC TRUE
 #endif
 
-// Использование DMA
+/* Set to TRUE enable DMA usage, FALSE disable it */
 #ifndef HAL_DMA
 #define HAL_DMA TRUE
 #endif
 
-// Использование Flash
+/* Set to TRUE enable Flash access, FALSE disable it */
 #ifndef HAL_FLASH
 #define HAL_FLASH TRUE
 #endif
 
-// Использование AES
+/* Set to TRUE enable AES usage, FALSE disable it */
 #ifndef HAL_AES
 #define HAL_AES TRUE
 #endif
@@ -421,10 +498,10 @@ st( \
 #define HAL_AES_DMA TRUE
 #endif
 
-// Использование LCD
+/* Set to TRUE enable LCD usage, FALSE disable it */
 #define HAL_LCD FALSE
 
-// Использование светодиодов
+/* Set to TRUE enable LED usage, FALSE disable it */
 #ifndef HAL_LED
 #define HAL_LED TRUE
 #endif
@@ -432,10 +509,10 @@ st( \
 #define BLINK_LEDS
 #endif
 
-// Использование кнопок
+/* Set to TRUE enable KEY usage, FALSE disable it */
 #define HAL_KEY FALSE
 
-// Использование UART
+/* Set to TRUE enable UART usage, FALSE disable it */
 #ifndef HAL_UART
 #if (defined ZAPP_P1) || (defined ZAPP_P2) || (defined ZTOOL_P1) || (defined ZTOOL_P2)
 #define HAL_UART TRUE
@@ -486,3 +563,5 @@ st( \
 /* USB is not used for CC2530 configuration */
 #define HAL_UART_USB  0
 #endif
+/*******************************************************************************************************
+*/
